6a2e1e34bf7a901574c23154ec9c1a54395d5fd5
Matěj Klimeček <matej.klimecek.025@pslib.cz>
2026-05-04 09:36:24 +0200

    added score system, pre-evaluation function

--- main.ts
+++ main.ts
@@ -3,23 +3,44 @@ enum state {
     started = 1,
     running = 2
 }
+let scoreA: number = 0;
+let scoreB: number = 0;
 let gameState: number = state.passive;
 let waitTime: number;
 let miliConvert: number = 1000;
 
+function preEval (a: boolean, b: boolean) {
+    if(a && b) {
+        basic.showIcon(IconNames.Sad)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
+        gameState = state.passive
+    } else if (a) {
+        control.runInBackground(() => music.playTone(600, 200))
+        basic.showString("B")
+        gameState = state.passive
+    } else if (b) {
+        control.runInBackground(() => music.playTone(600, 200))
+        basic.showString("A")
+        gameState = state.passive
+    }
+}
 function eval (a: boolean, b: boolean) {
     if(a && b){
         basic.showIcon(IconNames.Square)
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
         gameState = state.passive
         basic.pause(1000)
+        scoreA += 1
+        scoreB += 1
     }else if(a){
         control.runInBackground(() => music.playTone(600, 200))
         basic.showString("A")
+        scoreA += 1
         gameState = state.passive
     }else if(b){
         control.runInBackground(() => music.playTone(600, 200))
         basic.showString("B")
+        scoreB += 1
         gameState = state.passive
     }else {
         basic.pause(30)
@@ -60,27 +81,36 @@ input.onButtonPressed(Button.AB, function() {
         basic.pause(waitTime)
         pressedA = input.buttonIsPressed(Button.A)
         pressedB = input.buttonIsPressed(Button.B)
-        if (pressedA && pressedB) {
-            basic.showIcon(IconNames.Sad)
-            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
-            gameState = state.passive
-        } else if (pressedA) {
-            control.runInBackground(() => music.playTone(600, 200))
-            basic.showString("B")
-            gameState = state.passive
-        } else if (pressedB) {
-            control.runInBackground(() => music.playTone(600, 200))
-            basic.showString("A")
-            gameState = state.passive
-        } else {
-            gameState = state.running
-            control.runInBackground(() => music.playTone(222, 200))  // 440 Hz, 200 ms
-            basic.showIcon(IconNames.Pitchfork)
-            while(gameState === state.running){
-                pressedA = input.buttonIsPressed(Button.A)
-                pressedB = input.buttonIsPressed(Button.B)
-                eval(pressedA, pressedB)
-            }
+        preEval(pressedA, pressedB)
+        if(gameState === state.passive){
+            return;
         }
+        gameState = state.running
+        control.runInBackground(() => music.playTone(222, 200))  // 440 Hz, 200 ms
+        basic.showIcon(IconNames.Pitchfork)
+        while(gameState === state.running){
+            pressedA = input.buttonIsPressed(Button.A)
+            pressedB = input.buttonIsPressed(Button.B)
+            eval(pressedA, pressedB)
+        }
+    }
+})
+
+input.onButtonPressed(Button.A, function() {
+    if(gameState === state.passive){
+        whaleysans.showNumber(scoreA)
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+    if (gameState === state.passive) {
+        whaleysans.showNumber(scoreB)
+    }
+})
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function() {
+    if(gameState === state.passive){
+        scoreA = 0
+        scoreB = 0
     }
 })
